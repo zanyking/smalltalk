272b2306c8cd51f12f373d5b9d54265539af4d14
zanyking <zanyking@gmail.com>
2012-07-27 09:46:12 +0800

    first commit

---
+++ CDI_integration/backend_demo/src/main/java/demo/model/TestPE.java
@@ -0,0 +1,29 @@
+/* TestPE.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Jul 25, 2012, Created by Ian Tsai(Zanyking)
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+	This program is distributed under ZOL in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY.
+}}IS_RIGHT
+*/
+package demo.model;
+
+/**
+ * @author Ian Y.T Tsai(zanyking)
+ *
+ */
+public class TestPE {
+
+	
+	public void test(){
+	}
+	
+}
---
+++ CDI_integration/backend_demo/src/main/java/org/seamframework/tx/EntityTransactionInterceptor.java
@@ -0,0 +1,44 @@
+package org.seamframework.tx;
+
+import java.io.Serializable;
+
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+
+/**
+ * Declarative JPA EntityTransactions
+ * 
+ * @author Gavin King
+ */
+@Transactional
+@Interceptor
+public class EntityTransactionInterceptor implements Serializable {
+
+	private @Inject
+	@Any
+	EntityManager em;
+
+	@AroundInvoke
+	public Object aroundInvoke(InvocationContext ic) throws Exception {
+		boolean act = !em.getTransaction().isActive();
+		if (act) {
+			em.getTransaction().begin();
+		}
+		try {
+			Object result = ic.proceed();
+			if (act) {
+				em.getTransaction().commit();
+			}
+			return result;
+		} catch (Exception e) {
+			if (act) {
+				em.getTransaction().rollback();
+			}
+			throw e;
+		}
+	}
+}
